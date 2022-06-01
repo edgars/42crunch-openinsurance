0afa5a0ec942382fa369f32b3f0627bbc16f8ce5
Edgar Silva <edgar@skalena.com>
2022-06-01 15:37:34 -0300

    Fixing Scripts

--- openinsurance-br/canais-atendimento/demo-scripts/1-resetGithubActionsFile.sh
+++ openinsurance-br/canais-atendimento/demo-scripts/1-resetGithubActionsFile.sh
@@ -1,9 +1,5 @@
 clear
-echo 'Reseting Github Actions for accepting any audit score with any score'
+echo '🛡🎢 Reseting Github Actions for accepting any audit score with any score'
 cp ./templates/1-main.yaml ../../../.github/workflows/main.yaml
-cd ../../..
-pwd
-git add .
-git commit 'reseting the demo for accepting any audit score'
-git push
-pwd
+echo '🎉 Now time to see the changes via VS Code'
+
