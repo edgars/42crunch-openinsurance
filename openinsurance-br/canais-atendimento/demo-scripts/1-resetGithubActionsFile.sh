5d3f0ae9eac10147ef822a3ab1d069d2ccbd1e53
Edgar Silva <edgar@skalena.com>
2022-06-01 19:19:21 -0300

    Vai planeta

--- openinsurance-br/canais-atendimento/demo-scripts/1-resetGithubActionsFile.sh
+++ openinsurance-br/canais-atendimento/demo-scripts/1-resetGithubActionsFile.sh
@@ -3,8 +3,9 @@ echo '🛡🎢 Reseting Github Actions for accepting any audit score with any sc
 echo '✋ Please, remove the file ../../../.github/workflows/main.yml and tweak with it a little bit'
 
 cp ./templates/1-main.yaml ../../../.github/workflows/main.yml
+echo '😱 Git remove and add the new main.yml'
 git rm --cached ../../../.github/workflows/main.yml
 git add .
 
-echo '🎉 Now time to see the changes via VS Code and Stage them or use git commands in terminal 🎉'
+echo '🎉 Now time to see the changes via VS Code and Stage them or use git commit and push in terminal 🎉'
 
