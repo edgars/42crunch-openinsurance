ca4d76864afa641867579735f315bc0c62d28bcd
Edgar Silva <edgar@skalena.com>
2022-06-01 20:00:48 -0300

    New Score 65

--- openinsurance-br/canais-atendimento/demo-scripts/2-fixOpenSpec.sh
+++ openinsurance-br/canais-atendimento/demo-scripts/2-fixOpenSpec.sh
@@ -1,9 +1,9 @@
 clear
 echo '🛡🎢 Reseting Github Actions for accepting any audit score with any score'
-cp ./templates/2.fixed_api.txt ../../nodejs-server-server-generated/openapi.yaml
+cp ./templates/2.fixed_api.txt ../../canais-atendimento/nodejs-server-server-generated/api/openapi.yaml
 echo '✋ Please, removing the file ../../nodejs-server-server-generated/openapi.yaml'
-echo '😱 Git remove and add the new main.yml'
-git rm --cached ../../nodejs-server-server-generated/openapi.yaml
+echo '😱 Git remove and add the openapi.yaml'
+git rm --cached ../../canais-atendimento/nodejs-server-server-generated/api/openapi.yaml
 git add .
 
 echo '🎉 Now time to see the changes via VS Code and Stage them or use git commit and push in terminal 🎉'
