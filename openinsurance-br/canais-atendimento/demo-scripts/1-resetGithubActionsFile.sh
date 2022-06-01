2cc150e6ffe77214d3d379631c95757ea025852e
Edgar Silva <edgar@skalena.com>
2022-06-01 15:50:20 -0300

    One more

--- openinsurance-br/canais-atendimento/demo-scripts/1-resetGithubActionsFile.sh
+++ openinsurance-br/canais-atendimento/demo-scripts/1-resetGithubActionsFile.sh
@@ -2,5 +2,5 @@ clear
 echo '🛡🎢 Reseting Github Actions for accepting any audit score with any score'
 touch ../../../.github/workflows/main.yml
 cp ./templates/1-main.yaml ../../../.github/workflows/main.yml
-echo '🎉 Now time to see the changes via VS Code and Stage them or use git commands in terminal'
+echo '🎉 Now time to see the changes via VS Code and Stage them or use git commands in terminal 🎉'
 
